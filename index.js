878a5f8796ad6dfc81b4a29f4f38da5ccf2782cd
Sneha Majeti <majetisneha369@gmail.com>
2024-05-06 15:40:15 +0530

    increase limit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 
 app.get('/', (req,res)=>{
     res.send('Signer Gateway test app is running')
